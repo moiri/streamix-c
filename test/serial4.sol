a1260d223f62c0f751f07a183398015f4057d69d
Simon Maurer <moirelein@gmail.com>
2016-01-30 20:32:20 +0000

    fix test solutions (filer of line numbers)

--- test/serial4.sol
+++ test/serial4.sol
@@ -1,3 +1,3 @@
-test/serial4.test: 3: error: no port connection in serial combinition 'B(*).A(*)'
-test/serial4.test: 3: error: port 'a' in 'A'(A, *) is not connected
+test/serial4.test: *: error: no port connection in serial combinition 'B(*).A(*)'
+test/serial4.test: *: error: port 'a' in 'A'(A, *) is not connected
  Error count: 2
