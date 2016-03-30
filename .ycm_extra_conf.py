9b83a233b867c24a019c7060d5954ef0b8bce66a
Simon Maurer <moirelein@gmail.com>
2016-03-30 10:48:41 +0100

    DEBUG is redefined when enabled in defines.h

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -48,7 +48,6 @@ flags = [
 '-DDOT_COLOR',
 '-DDOT_EDGE_LABEL',
 '-DDOT_SYNC_FIRST',
-'-DDEBUG',
 # You 100% do NOT need -DUSE_CLANG_COMPLETER in your flags; only the YCM
 # source code needs it.
 '-DUSE_CLANG_COMPLETER',
