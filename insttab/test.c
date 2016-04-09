7e862a3f106338c1cda9cb26ae327eb625b4c274
Simon Maurer <moirelein@gmail.com>
2016-04-09 14:09:58 +0100

    rest removing of records

--- insttab/test.c
+++ insttab/test.c
@@ -9,23 +9,25 @@ int main( ) {
     int error_cnt = 0;
     int scope = 5;
     int id = 10;
+    int id_alt = 11;
     char name[] = "test";
 
     inst_net_put( &nets, scope );
 
+    // add net and check wheter it was added
     res1 = inst_net_get( &nets, scope );
     if( res1 == NULL ) {
         printf( "error: no nets in scope %d found\n", scope );
-        error_cnt++;
         return 0;
     }
     else if( res1->scope != scope ) {
         printf( "error: expected scope=%d but got scope=%d\n", scope,
                 res1->scope );
-        error_cnt++;
     }
+    else printf( "success: add net\n" );
 
-    inst_rec_put( &res1->recs_name, &res1->recs_id, name, id, NULL );
+    // add rec and check if we can get it by name and by id
+    inst_rec_put( &res1->recs_name, &res1->recs_id, name, id, 0, 0, NULL );
     res2 = inst_rec_get_id( &res1->recs_id, id );
     if( res2 == NULL ) {
         printf( "error: no record found with id '%d'\n", id );
@@ -42,6 +44,12 @@ int main( ) {
             error_cnt++;
         }
     }
+    if( error_cnt == 0 ) {
+        printf( "success: add rec name=%s, id=%d\n", name, id );
+        printf( "success: get rec by id=%d\n", id );
+    }
+    error_cnt = 0;
+
     res2 = inst_rec_get_name( &res1->recs_name, name );
     if( res2 == NULL ) {
         printf( "error: no record found with name '%s'\n", name );
@@ -58,8 +66,88 @@ int main( ) {
             error_cnt++;
         }
     }
+    if( error_cnt == 0 ) printf( "success: get rec by name=%s\n", name );
+    error_cnt = 0;
 
-    if( error_cnt == 0 ) printf( "success!\n" );
+    // add another rec with the same name as the previous record and check if we
+    // can get it by name and by id
+    inst_rec_put( &res1->recs_name, &res1->recs_id, name, id_alt, 0, 0, NULL );
+    res2 = inst_rec_get_id( &res1->recs_id, id_alt );
+    if( res2 == NULL ) {
+        printf( "error: no record found with id '%d'\n", id_alt );
+        error_cnt++;
+    }
+    else {
+        if( res2->id != id_alt ) {
+            printf( "error: expected id=%d but got id=%d\n", id_alt, res2->id );
+            error_cnt++;
+        }
+        if( strcmp( res2->name, name ) != 0 ) {
+            printf( "error: expected name=%s but got name=%s\n", name,
+                    res2->name );
+            error_cnt++;
+        }
+    }
+    if( error_cnt == 0 ) {
+        printf( "success: add rec collision, name=%s, id=%d\n", name, id_alt );
+        printf( "success: get rec by id=%d\n", id_alt );
+    }
+    error_cnt = 0;
+
+    res2 = inst_rec_get_name( &res1->recs_name, name );
+    if( res2 == NULL ) {
+        printf( "error: no record found with name '%s'\n", name );
+        error_cnt++;
+    }
+    else {
+        if( strcmp( res2->name, name ) != 0 ) {
+            printf( "error: expected name=%s but got name=%s\n", name,
+                    res2->name );
+            error_cnt++;
+        }
+        while( res2 != NULL ) {
+            if( res2->id == id_alt ) break;
+            res2 = res2->next;
+        }
+        if( res2 == NULL ) {
+            printf( "error: record with id=%d not found\n", id_alt );
+            error_cnt++;
+        }
+    }
+    if( error_cnt == 0 )
+        printf( "success: get rec by name=%s (id=%d)\n", name, id_alt );
+    error_cnt = 0;
+
+    // remove element that was added second
+    inst_rec_del( &res1->recs_name, &res1->recs_id, res2 );
+    res2 = inst_rec_get_id( &res1->recs_id, id_alt );
+    if( res2 != NULL ) {
+        printf( "error: element name=%s, id=%d was not deleted\n", res2->name,
+                res2->id );
+        error_cnt++;
+    }
+    if( error_cnt == 0 )
+        printf( "success: del rec, name=%s, id=%d\n", name, id_alt );
+
+    res2 = inst_rec_get_name( &res1->recs_name, name );
+    if( res2 == NULL ) {
+        printf( "error: no record found with name '%s'\n", name );
+        error_cnt++;
+    }
+    else {
+        if( res2->id != id ) {
+            printf( "error: expected id=%d but got id=%d\n", id, res2->id );
+            error_cnt++;
+        }
+        if( strcmp( res2->name, name ) != 0 ) {
+            printf( "error: expected name=%s but got name=%s\n", name,
+                    res2->name );
+            error_cnt++;
+        }
+    }
+    if( error_cnt == 0 )
+        printf( "success: get rec by name=%s (id=%d)\n", name, id );
+
 
     return 0;
 }
