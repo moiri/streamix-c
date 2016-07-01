a821c15e874488be9f814fbd0f67402ba61329cf
Simon Maurer <moirelein@gmail.com>
2016-07-01 18:41:07 +0100

    Change structure and typdef names according to a standard
     - Use a _s postfix for structures
     - Use a _t postfix for types
     - Fix comments according to Doxygen syntax
     - Add missing comments

--- include/vnet.h
+++ include/vnet.h
@@ -1,11 +1,11 @@
-/* 
+/**
  * A library to handle virtual net interfaces, i.e. the interface
  * of a resulting net of an operator
  *
  * @file    vnet.h
  * @author  Simon Maurer
  *
- * */
+ */
 
 #ifndef VNET_H
 #define VNET_H
@@ -14,81 +14,107 @@
 #include "symtab.h"
 #include "insttab.h"
 
-typedef struct net_con net_con;
-typedef struct virt_net virt_net;
-typedef struct virt_ports virt_ports;
+// TYPEDEFS -------------------------------------------------------------------
+typedef struct net_con_s net_con_t;
+typedef struct virt_net_s virt_net_t;
+typedef struct virt_port_s virt_port_t;
 
-// vectors to store the connection ids
-struct net_con
+// STRUCTS --------------------------------------------------------------------
+/**
+ * @brief   vectors to store the connection ids
+ */
+struct net_con_s
 {
-    igraph_vector_ptr_t left;
-    igraph_vector_ptr_t right;
+    igraph_vector_ptr_t left;   /**< left connection vector */
+    igraph_vector_ptr_t right;  /**< right connection vector */
 };
 
-// a virtual net used as an intermediate interface to construct a net out of a
-// network equation
-struct virt_net
+/**
+ * @brief   structure of a virtual net
+ *
+ * a virtual net used as an intermediate interface to construct a net out of a
+ * network equation
+ */
+struct virt_net_s
 {
-    net_con*    con;
-    virt_ports* ports;
+    net_con_t*      con;    /**< connection vector structure */
+    virt_port_t*    ports;  /**< port list */
 };
 
-// port list of the virtual net
-struct virt_ports
+/**
+ * @brief   port list of the virtual net
+ */
+struct virt_port_s
 {
-    symrec*     rec;        // pointer to port symbol (if declared)
-    inst_rec*   inst;       // pointer to net or cp-sync instance
-    int         attr_class; // updated class (VAL_NONE can be overwritten)
-    int         attr_mode;  // updated mode for cp-sync (VAL_BI)
-    virt_ports* next;
+    symrec*         rec;        /**< pointer to port symbol (if declared) */
+    inst_rec*       inst;       /**< pointer to net or cp-sync instance */
+    int             attr_class; /**< updated class (VAL_NONE can be changed) */
+    int             attr_mode;  /**< updated mode for cp-sync (VAL_BI) */
+    virt_port_t* next;
 };
 
-/* FUNCTION PROTOTYPES                                                        */
-/******************************************************************************/
+// FUNCTIONS ------------------------------------------------------------------
 
 /**
- * Create a new virtual net
+ * @brief   Create a new virtual net
  *
- * @param symrec*       pointer to a record from the symbol table
- * @param inst_rec*     pointer to a record of the instance table
- * @return virt_net*    pointer to the newly created virtual net
+ * @param rec   pointer to a record from the symbol table
+ * @param inst  pointer to a record of the instance table
+ * @return      pointer to the newly created virtual net
  */
-virt_net* virt_net_create( symrec*, inst_rec* );
+virt_net_t* virt_net_create( symrec*, inst_rec* );
 
 /**
- * Destroy a virtual net and its conent.
+ * @brief   Create a virtual net from two operands op1|op2
  *
- * @param virt_net*     pointer to th evirtual net
+ * Create a new virtial net from two parallel nets, following the
+ * parallel connection semantics of Streamix
+ *
+ * @param v_net1    pointer to virtual net of left operand
+ * @param v_net2    pointer to virtual net of right operand
+ * @return          pointer to the new virtual net
  */
-void virt_net_destroy( virt_net* );
+virt_net_t* virt_net_create_parallel( virt_net_t*, virt_net_t* );
 
 /**
- * Destroy a the structure of virtual net but not its content.
+ * @brief   Create a virtual net from two operands op1.op2
  *
- * @param virt_net*     pointer to th evirtual net
+ * Create a new virtial net from two serial nets, following the
+ * serial connection semantics of Streamix
+ *
+ * @param v_net1    pointer to virtual net of left operand
+ * @param v_net2    pointer to virtual net of right operand
+ * @return          pointer to the new virtual net
  */
-void virt_net_destroy_struct( virt_net* );
+virt_net_t* virt_net_create_serial( virt_net_t*, virt_net_t* );
 
 /**
- * Change merge two virtual nets into one, following the
- * parallel connection semantics
+ * @brief   Make a deep copy of a virtual net
  *
- * @param virt_net*     pointer to virtual net of left operand
- * @param virt_net*     pointer to virtual net of right operand
+ * Create a deep copy from a virtual net. Note that all all ports of a virtual
+ * net only point to the port record in the symbol table. Hence only the
+ * pointer list is copied but not the port record itself
+ *
+ * @param v_net pointer to a virtual net
+ * @return      pointer to the new virtual net
  */
-virt_net* virt_net_merge_parallel( virt_net* , virt_net* );
+virt_net_t* virt_net_copy( virt_net_t* );
 
 /**
- * Change merge two virtual nets into one, following the
- * serial connection semantics
+ * @brief   Make a copy of the port list in a virtual net
  *
- * @param virt_net*     pointer to virtual net of left operand
- * @param virt_net*     pointer to virtual net of right operand
+ * @param old   a pointer to the port list to be copied
+ * @param last  a pointer to the last element of a potential other list
+ * @param class force a class to each port in the list
+ *              if set to -1 this is ignored
+ * @return      pointer to the last element of the new list
  */
-virt_net* virt_net_merge_serial( virt_net* , virt_net* );
+virt_port_t* virt_net_copy_ports( virt_port_t*, virt_port_t*, int );
 
-virt_net* virt_net_create_parallel( virt_net*, virt_net* );
-virt_net* virt_net_create_serial( virt_net*, virt_net* );
-virt_net* virt_net_copy( virt_net* );
-virt_ports* virt_net_copy_ports( virt_ports*, virt_ports*, int );
+/**
+ * @brief   Destroy a virtual net and its conent.
+ *
+ * @param v_net pointer to the virtual net
+ */
+void virt_net_destroy( virt_net_t* );
 #endif // VNET_H
--- src/vnet.c
+++ src/vnet.c
@@ -3,19 +3,19 @@
 #include "ast.h"
 
 /******************************************************************************/
-virt_net* virt_net_create( symrec* rec, inst_rec* inst )
+virt_net_t* virt_net_create( symrec* rec, inst_rec* inst )
 {
     symrec_list* ports_ptr = NULL;
-    virt_net* v_net = NULL;
-    virt_ports* ports = NULL;
-    virt_ports* ports_last = NULL;
+    virt_net_t* v_net = NULL;
+    virt_port_t* ports = NULL;
+    virt_port_t* ports_last = NULL;
 
     if( rec->type == AST_BOX )
         ports_ptr = ( ( struct box_attr* )rec->attr )->ports;
     else if( rec->type == AST_WRAP )
         ports_ptr = ( ( struct wrap_attr* )rec->attr )->ports;
     while( ports_ptr != NULL  ) {
-        ports = malloc( sizeof( virt_ports ) );
+        ports = malloc( sizeof( virt_port_t ) );
         ports->rec = ports_ptr->rec;
         ports->next = ports_last;
         ports->attr_class =
@@ -26,9 +26,9 @@ virt_net* virt_net_create( symrec* rec, inst_rec* inst )
         ports_last = ports;
         ports_ptr = ports_ptr->next;
     }
-    v_net = malloc( sizeof( virt_net ) );
+    v_net = malloc( sizeof( virt_net_t ) );
     v_net->ports = ports_last;
-    v_net->con = malloc( sizeof( net_con ) );
+    v_net->con = malloc( sizeof( net_con_t ) );
     igraph_vector_ptr_init( &v_net->con->left, 1 );
     VECTOR( v_net->con->left )[ 0 ] = &inst->id;
     igraph_vector_ptr_copy( &v_net->con->right, &v_net->con->left );
@@ -37,102 +37,18 @@ virt_net* virt_net_create( symrec* rec, inst_rec* inst )
 }
 
 /******************************************************************************/
-void virt_net_destroy( virt_net* v_net )
+virt_net_t* virt_net_create_parallel( virt_net_t* v_net1, virt_net_t* v_net2 )
 {
-    virt_ports* ports = NULL;
-    // free ports
-    while( v_net->ports != NULL ) {
-        ports = v_net->ports;
-        v_net->ports = v_net->ports->next;
-        free( ports );
-    }
-    // free connection vectors
-    igraph_vector_ptr_destroy( &v_net->con->left );
-    igraph_vector_ptr_destroy( &v_net->con->right );
-    virt_net_destroy_struct( v_net );
-}
-
-/******************************************************************************/
-void virt_net_destroy_struct( virt_net* v_net )
-{
-    // free structures
-    free( v_net->con );
-    free( v_net );
-}
-
-/******************************************************************************/
-virt_net* virt_net_merge_parallel( virt_net* v_net1, virt_net* v_net2 )
-{
-    virt_ports* ports = NULL;
-    virt_ports* ports_last = NULL;
-
-    // alter port list
-    ports = v_net1->ports;
-    while( ports != NULL ) {
-        ports_last = ports;
-        ports = ports->next;
-    }
-    ports_last->next = v_net2->ports;
-
-    // alter connection lists
-    igraph_vector_ptr_append( &v_net1->con->left, &v_net2->con->left );
-    igraph_vector_ptr_append( &v_net1->con->right, &v_net2->con->right );
-
-    // destroy obsolete connection vectors
-    igraph_vector_ptr_destroy( &v_net2->con->left );
-    igraph_vector_ptr_destroy( &v_net2->con->right );
-
-    virt_net_destroy_struct( v_net2 );
-
-    return v_net1;
-}
-
-/******************************************************************************/
-virt_net* virt_net_merge_serial( virt_net* v_net1, virt_net* v_net2 )
-{
-    virt_ports* ports = NULL;
-    virt_ports* ports_last = NULL;
-
-    // alter ports
-    ports = v_net1->ports;
-    while( ports != NULL ) {
-        if( ports->attr_class != VAL_SIDE ) ports->attr_class = VAL_UP;
-        ports_last = ports;
-        ports = ports->next;
-    }
-    if( ports_last != NULL ) ports_last->next = v_net2->ports;
-    else v_net1->ports = v_net2->ports;
-    ports = v_net2->ports;
-    while( ports != NULL ) {
-        if(ports->attr_class != VAL_SIDE ) ports->attr_class = VAL_DOWN;
-        ports = ports->next;
-    }
-
-    // destroy obsolete connection vectors
-    igraph_vector_ptr_destroy( &v_net1->con->right );
-    igraph_vector_ptr_destroy( &v_net2->con->left );
-
-    // alter connection vectors of virtual net
-    v_net1->con->right = v_net2->con->right;
-
-    virt_net_destroy_struct( v_net2 );
-
-    return v_net1;
-}
-
-/******************************************************************************/
-virt_net* virt_net_create_parallel( virt_net* v_net1, virt_net* v_net2 )
-{
-    virt_net* v_net = NULL;
-    virt_ports* ports1 = NULL;
-    virt_ports* ports2 = NULL;
+    virt_net_t* v_net = NULL;
+    virt_port_t* ports1 = NULL;
+    virt_port_t* ports2 = NULL;
 
     // alter ports
     ports1 = virt_net_copy_ports( v_net1->ports, NULL, -1 );
     ports2 = virt_net_copy_ports( v_net2->ports, ports1, -1 );
-    v_net = malloc( sizeof( virt_net ) );
+    v_net = malloc( sizeof( virt_net_t ) );
     v_net->ports = ports2;
-    v_net->con = malloc( sizeof( net_con ) );
+    v_net->con = malloc( sizeof( net_con_t ) );
     igraph_vector_ptr_copy( &v_net->con->left, &v_net1->con->left );
     igraph_vector_ptr_append( &v_net->con->left, &v_net2->con->left );
     igraph_vector_ptr_copy( &v_net->con->right, &v_net1->con->right );
@@ -142,18 +58,18 @@ virt_net* virt_net_create_parallel( virt_net* v_net1, virt_net* v_net2 )
 }
 
 /******************************************************************************/
-virt_net* virt_net_create_serial( virt_net* v_net1, virt_net* v_net2 )
+virt_net_t* virt_net_create_serial( virt_net_t* v_net1, virt_net_t* v_net2 )
 {
-    virt_net* v_net = NULL;
-    virt_ports* ports1 = NULL;
-    virt_ports* ports2 = NULL;
+    virt_net_t* v_net = NULL;
+    virt_port_t* ports1 = NULL;
+    virt_port_t* ports2 = NULL;
 
     // alter ports
     ports1 = virt_net_copy_ports( v_net1->ports, NULL, VAL_UP );
     ports2 = virt_net_copy_ports( v_net2->ports, ports1, VAL_DOWN );
-    v_net = malloc( sizeof( virt_net ) );
+    v_net = malloc( sizeof( virt_net_t ) );
     v_net->ports = ports2;
-    v_net->con = malloc( sizeof( net_con ) );
+    v_net->con = malloc( sizeof( net_con_t ) );
     igraph_vector_ptr_copy( &v_net->con->left, &v_net1->con->left );
     igraph_vector_ptr_copy( &v_net->con->right, &v_net2->con->right );
 
@@ -161,39 +77,59 @@ virt_net* virt_net_create_serial( virt_net* v_net1, virt_net* v_net2 )
 }
 
 /******************************************************************************/
-virt_net* virt_net_copy( virt_net* v_net_old )
+virt_net_t* virt_net_copy( virt_net_t* v_net )
 {
-    virt_net* v_net = NULL;
-    virt_ports* ports = NULL;
+    virt_net_t* v_net_new = NULL;
+    virt_port_t* ports = NULL;
 
     // alter ports
-    ports = virt_net_copy_ports( v_net_old->ports, NULL, -1 );
-    v_net = malloc( sizeof( virt_net ) );
-    v_net->ports = ports;
-    v_net->con = malloc( sizeof( net_con ) );
-    igraph_vector_ptr_copy( &v_net->con->left, &v_net_old->con->left );
-    igraph_vector_ptr_copy( &v_net->con->right, &v_net_old->con->right );
+    ports = virt_net_copy_ports( v_net->ports, NULL, -1 );
+    v_net_new = malloc( sizeof( virt_net_t ) );
+    v_net_new->ports = ports;
+    v_net_new->con = malloc( sizeof( net_con_t ) );
+    igraph_vector_ptr_copy( &v_net_new->con->left, &v_net->con->left );
+    igraph_vector_ptr_copy( &v_net_new->con->right, &v_net->con->right );
 
-    return v_net;
+    return v_net_new;
 }
 
 /******************************************************************************/
-virt_ports* virt_net_copy_ports( virt_ports* old, virt_ports* last, int class )
-    {
-        virt_ports* new = NULL;
+virt_port_t* virt_net_copy_ports( virt_port_t* old, virt_port_t* last,
+        int class )
+{
+    virt_port_t* new = NULL;
 
-        while( old != NULL ) {
-            new = malloc( sizeof( virt_ports ) );
-            new->rec = old->rec;
-            new->inst = old->inst;
-            new->next = last;
-            if( ( old->attr_class != VAL_SIDE ) && ( class >= 0 ) )
-                new->attr_class = class;
-            else new->attr_class = old->attr_class;
-            new->attr_mode = old->attr_mode;
-            last = new;
-            old = old->next;
-        }
-
-        return last;
+    while( old != NULL ) {
+        new = malloc( sizeof( virt_port_t ) );
+        new->rec = old->rec;
+        new->inst = old->inst;
+        new->next = last;
+        if( ( old->attr_class != VAL_SIDE ) && ( class >= 0 ) )
+            new->attr_class = class;
+        else new->attr_class = old->attr_class;
+        new->attr_mode = old->attr_mode;
+        last = new;
+        old = old->next;
     }
+
+    return last;
+}
+
+/******************************************************************************/
+void virt_net_destroy( virt_net_t* v_net )
+{
+    virt_port_t* ports = NULL;
+    // free ports
+    while( v_net->ports != NULL ) {
+        ports = v_net->ports;
+        v_net->ports = v_net->ports->next;
+        free( ports );
+    }
+    // free connection vectors
+    igraph_vector_ptr_destroy( &v_net->con->left );
+    igraph_vector_ptr_destroy( &v_net->con->right );
+
+    // free structures
+    free( v_net->con );
+    free( v_net );
+}
