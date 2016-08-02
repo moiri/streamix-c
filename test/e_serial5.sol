f27e78ea7723cc02bde9a0ea804425328ab3ce06
Simon Maurer <moirelein@gmail.com>
2016-08-02 17:26:16 +0100

    renaming and commenting some test cases

--- test/e_serial5.sol
+++
@@ -1,3 +0,0 @@
-test/e_serial5.smx: 6: error: no port connection in serial combinition 'A1(*).B2(*)'
-test/e_serial5.smx: 6: error: no port connection in serial combinition 'A2(*).B1(*)'
- Error count: 2
