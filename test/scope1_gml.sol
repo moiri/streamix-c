ebd27a7d299808da812044cd2638f4db7cec9f9d
Simon Maurer <moirelein@gmail.com>
2016-08-12 18:01:52 +0100

    provide sol files and comments for scope tcs

---
+++ test/scope1_gml.sol
@@ -0,0 +1,48 @@
+Creator "igraph version 0.7.1 StreamixC"
+Version 1
+graph
+[
+  directed 1
+  node
+  [
+    id 0
+    label "A"
+    func "A"
+  ]
+  node
+  [
+    id 1
+    label "B"
+    func "B"
+  ]
+  node
+  [
+    id 2
+    label "A"
+    func "A"
+  ]
+  node
+  [
+    id 3
+    label "B"
+    func "B"
+  ]
+  edge
+  [
+    source 0
+    target 1
+    label "a"
+  ]
+  edge
+  [
+    source 2
+    target 3
+    label "b"
+  ]
+  edge
+  [
+    source 1
+    target 2
+    label "p"
+  ]
+]
---
+++ test/scope2_gml.sol
@@ -0,0 +1,60 @@
+Creator "igraph version 0.7.1 StreamixC"
+Version 1
+graph
+[
+  directed 1
+  node
+  [
+    id 0
+    label "A"
+    func "A"
+  ]
+  node
+  [
+    id 1
+    label "B"
+    func "B"
+  ]
+  node
+  [
+    id 2
+    label "A"
+    func "A"
+  ]
+  node
+  [
+    id 3
+    label "B"
+    func "B"
+  ]
+  node
+  [
+    id 4
+    label "W"
+    func "W"
+  ]
+  edge
+  [
+    source 0
+    target 1
+    label "a"
+  ]
+  edge
+  [
+    source 2
+    target 3
+    label "b"
+  ]
+  edge
+  [
+    source 3
+    target 4
+    label "c"
+  ]
+  edge
+  [
+    source 1
+    target 2
+    label "p"
+  ]
+]
