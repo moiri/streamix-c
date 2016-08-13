914926aee6a93025def0f0d3be24cf6aa093116d
Simon Maurer <moirelein@gmail.com>
2016-08-13 21:38:28 +0100

    sol files for hierarchy tcs

---
+++ test/hierarchy15_gml.sol
@@ -0,0 +1,90 @@
+Creator "igraph version 0.7.1 StreamixC"
+Version 1
+graph
+[
+  directed 1
+  node
+  [
+    id 0
+    label "A"
+    func "A"
+  ]
+  node
+  [
+    id 1
+    label "B"
+    func "B"
+  ]
+  node
+  [
+    id 2
+    label "CA"
+    func "CA"
+  ]
+  node
+  [
+    id 3
+    label "CC"
+    func "CC"
+  ]
+  node
+  [
+    id 4
+    label "CD"
+    func "CD"
+  ]
+  node
+  [
+    id 5
+    label "CBA"
+    func "CBA"
+  ]
+  node
+  [
+    id 6
+    label "CBB"
+    func "CBB"
+  ]
+  edge
+  [
+    source 3
+    target 4
+    label "cb_int2"
+  ]
+  edge
+  [
+    source 5
+    target 6
+    label "cbb"
+  ]
+  edge
+  [
+    source 2
+    target 5
+    label "cb_int1"
+  ]
+  edge
+  [
+    source 6
+    target 1
+    label "b1"
+  ]
+  edge
+  [
+    source 4
+    target 1
+    label "b2"
+  ]
+  edge
+  [
+    source 0
+    target 3
+    label "a2"
+  ]
+  edge
+  [
+    source 0
+    target 2
+    label "a1"
+  ]
+]
