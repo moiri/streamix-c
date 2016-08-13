e803038c0759c0bf9bc6da03f3b607d7eb4e86f1
Simon Maurer <moirelein@gmail.com>
2016-08-13 12:22:14 +0100

    add another bunch of hierchy testcases

---
+++ test/hierarchy4_gml.sol
@@ -0,0 +1,90 @@
+Creator "igraph version 0.7.1 StreamixC"
+Version 1
+graph
+[
+  directed 1
+  node
+  [
+    id 0
+    label "A"
+    func "A"
+  ]
+  node
+  [
+    id 1
+    label "A1"
+    func "A1"
+  ]
+  node
+  [
+    id 2
+    label "A1"
+    func "A1"
+  ]
+  node
+  [
+    id 3
+    label "smx_cp"
+    func "null"
+  ]
+  node
+  [
+    id 4
+    label "smx_cp"
+    func "null"
+  ]
+  node
+  [
+    id 5
+    label "A2"
+    func "A2"
+  ]
+  node
+  [
+    id 6
+    label "A2"
+    func "A2"
+  ]
+  edge
+  [
+    source 2
+    target 3
+    label "b"
+  ]
+  edge
+  [
+    source 1
+    target 3
+    label "b"
+  ]
+  edge
+  [
+    source 4
+    target 2
+    label "a"
+  ]
+  edge
+  [
+    source 4
+    target 1
+    label "a"
+  ]
+  edge
+  [
+    source 3
+    target 6
+    label "b"
+  ]
+  edge
+  [
+    source 3
+    target 5
+    label "b"
+  ]
+  edge
+  [
+    source 0
+    target 4
+    label "a"
+  ]
+]
---
+++ test/hierarchy6_gml.sol
@@ -0,0 +1,72 @@
+Creator "igraph version 0.7.1 StreamixC"
+Version 1
+graph
+[
+  directed 1
+  node
+  [
+    id 0
+    label "A"
+    func "A"
+  ]
+  node
+  [
+    id 1
+    label "A11"
+    func "A1"
+  ]
+  node
+  [
+    id 2
+    label "A12"
+    func "A1"
+  ]
+  node
+  [
+    id 3
+    label "A13"
+    func "A1"
+  ]
+  node
+  [
+    id 4
+    label "A21"
+    func "A2"
+  ]
+  node
+  [
+    id 5
+    label "A22"
+    func "A2"
+  ]
+  edge
+  [
+    source 1
+    target 2
+    label "b"
+  ]
+  edge
+  [
+    source 4
+    target 5
+    label "d"
+  ]
+  edge
+  [
+    source 5
+    target 3
+    label "e"
+  ]
+  edge
+  [
+    source 2
+    target 4
+    label "c"
+  ]
+  edge
+  [
+    source 0
+    target 1
+    label "a"
+  ]
+]
