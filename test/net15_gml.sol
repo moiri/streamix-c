b8bdc07d2e43a9f774f9b68265df2174e1c73f7a
Simon Maurer <moirelein@gmail.com>
2016-09-01 18:56:03 +0100

    new tc for sideports in nets

---
+++ test/net15_gml.sol
@@ -0,0 +1,48 @@
+Creator "igraph version 0.7.1 StreamixC"
+Version 1
+graph
+[
+  directed 1
+  node
+  [
+    id 0
+    label "smx_cp"
+    func "null"
+  ]
+  node
+  [
+    id 1
+    label "A"
+    func "A"
+  ]
+  node
+  [
+    id 2
+    label "A"
+    func "A"
+  ]
+  node
+  [
+    id 3
+    label "B"
+    func "B"
+  ]
+  edge
+  [
+    source 1
+    target 0
+    label "x"
+  ]
+  edge
+  [
+    source 2
+    target 0
+    label "x"
+  ]
+  edge
+  [
+    source 0
+    target 3
+    label "x"
+  ]
+]
