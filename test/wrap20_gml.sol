68b4fe6a91e35e19bff0d4361cf128665499e410
Simon Maurer <moirelein@gmail.com>
2016-09-01 18:40:09 +0100

    new testcase of multiple instances of one wrapper

---
+++ test/wrap20_gml.sol
@@ -0,0 +1,216 @@
+Creator "igraph version 0.7.1 StreamixC"
+Version 1
+graph
+[
+  directed 1
+  node
+  [
+    id 0
+    label "L"
+    func "L"
+  ]
+  node
+  [
+    id 1
+    label "R"
+    func "R"
+  ]
+  node
+  [
+    id 2
+    label "A"
+    func "A"
+  ]
+  node
+  [
+    id 3
+    label "A"
+    func "A"
+  ]
+  node
+  [
+    id 4
+    label "smx_cp"
+    func "null"
+  ]
+  node
+  [
+    id 5
+    label "smx_cp"
+    func "null"
+  ]
+  node
+  [
+    id 6
+    label "B"
+    func "B"
+  ]
+  node
+  [
+    id 7
+    label "B"
+    func "B"
+  ]
+  node
+  [
+    id 8
+    label "smx_cp"
+    func "null"
+  ]
+  node
+  [
+    id 9
+    label "A"
+    func "A"
+  ]
+  node
+  [
+    id 10
+    label "A"
+    func "A"
+  ]
+  node
+  [
+    id 11
+    label "smx_cp"
+    func "null"
+  ]
+  node
+  [
+    id 12
+    label "B"
+    func "B"
+  ]
+  node
+  [
+    id 13
+    label "B"
+    func "B"
+  ]
+  node
+  [
+    id 14
+    label "smx_cp"
+    func "null"
+  ]
+  edge
+  [
+    source 4
+    target 3
+    label "a"
+  ]
+  edge
+  [
+    source 4
+    target 2
+    label "a"
+  ]
+  edge
+  [
+    source 3
+    target 5
+    label "p_int"
+  ]
+  edge
+  [
+    source 2
+    target 5
+    label "p_int"
+  ]
+  edge
+  [
+    source 5
+    target 7
+    label "p_int"
+  ]
+  edge
+  [
+    source 5
+    target 6
+    label "p_int"
+  ]
+  edge
+  [
+    source 7
+    target 8
+    label "b"
+  ]
+  edge
+  [
+    source 6
+    target 8
+    label "b"
+  ]
+  edge
+  [
+    source 0
+    target 4
+    label "p1"
+  ]
+  edge
+  [
+    source 0
+    target 4
+    label "p2"
+  ]
+  edge
+  [
+    source 8
+    target 10
+    label "a"
+  ]
+  edge
+  [
+    source 8
+    target 9
+    label "a"
+  ]
+  edge
+  [
+    source 10
+    target 11
+    label "p_int"
+  ]
+  edge
+  [
+    source 9
+    target 11
+    label "p_int"
+  ]
+  edge
+  [
+    source 11
+    target 13
+    label "p_int"
+  ]
+  edge
+  [
+    source 11
+    target 12
+    label "p_int"
+  ]
+  edge
+  [
+    source 13
+    target 14
+    label "b"
+  ]
+  edge
+  [
+    source 12
+    target 14
+    label "b"
+  ]
+  edge
+  [
+    source 14
+    target 1
+    label "p2"
+  ]
+  edge
+  [
+    source 14
+    target 1
+    label "p1"
+  ]
+]
