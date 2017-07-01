753c82b622ae6125bd136c4d01ecc00992e6b5aa
Simon Maurer <moirelein@gmail.com>
2017-07-01 20:24:38 +0100

    two test cases to test the new deterministic operator

---
+++ test/con13_n_gml.sol
@@ -0,0 +1,39 @@
+Creator "igraph version 0.7.1 StreamixC"
+Version 1
+graph
+[
+  directed 1
+  node
+  [
+    id 0
+    label "A"
+    func "fa"
+    static 0
+    pure 0
+  ]
+  node
+  [
+    id 1
+    label "A"
+    func "fa"
+    static 0
+    pure 0
+  ]
+  node
+  [
+    id 2
+    label "B"
+    func "fb"
+    static 0
+    pure 0
+  ]
+  edge
+  [
+    source 1
+    target 2
+    label "x"
+    dsrc 0
+    ddst 0
+    len 1
+  ]
+]
