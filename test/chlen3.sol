483844a7860797ee9029ee89d1117653012d966f
Simon Maurer <simon.maurer@humdek.unibe.ch>
2019-09-20 17:38:07 +0200

    fix testcase: add warning messages when changing ch length

---
+++ test/chlen3.sol
@@ -0,0 +1,3 @@
+test/chlen3.smx: 5: warning: operator tt resets buffer size of port 'x' in 'B(*)' from 4 to 1.
+test/chlen3.smx: 5: warning: operator tt resets buffer size of port 'x' in 'B(*)' from 4 to 1.
+ Error count: 2
